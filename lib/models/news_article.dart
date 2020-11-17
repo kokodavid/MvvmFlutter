10c261448037150adf8ce8ba58f12fbf594b8326
kokodavid <kingkgdave@gmail.com>
2020-11-17 14:52:26 +0300

    news models

---
+++ lib/models/news_article.dart
@@ -0,0 +1,29 @@
+class NewsArticle {
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final String publishedAt;
+  final String content;
+
+  NewsArticle({
+  this.author,
+  this.title,
+  this.description,
+  this.url,
+  this.urlToImage,
+  this.publishedAt,
+  this.content
+});
+
+
+  factory NewsArticle.fromJson(Map<String, dynamic> json) {
+  return NewsArticle(
+    title: json['title'],
+
+
+  );
+  }
+}
+
