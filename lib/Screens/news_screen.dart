9d389aaaab32f271ccb523fabf7bf1f31ff1a054
kokodavid <kingkgdave@gmail.com>
2020-11-18 12:43:58 +0300

    news models

--- lib/Screens/news_screen.dart
+++ lib/Screens/news_screen.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:newsapp/viewModels/news_article_list_view_model.dart';
+import 'package:newsapp/widgets/news_grid.dart';
 import 'package:provider/provider.dart';
 
 class NewsScreen extends StatefulWidget {
@@ -20,15 +22,45 @@ class _NewsScreenState extends State<NewsScreen> {
     var listVieModel = Provider.of<NewsArticleListViewModel>(context);
 
     return Scaffold(
-      appBar: AppBar(),
-      body: GridView.builder(
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (context, index) {
-          return Text(listVieModel.articles[index].title);
-        },
-        itemCount: listVieModel.articles.length,
+      appBar: AppBar(
+        actions: <Widget>[
+          Icon(Icons.more_vert)
+        ],
       ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+         children: <Widget>[
+           Padding(
+             padding: const EdgeInsets.only(left:30),
+             child: Text("News",style: TextStyle(
+               fontWeight: FontWeight.bold,
+               fontSize: 45
+             ),
+             ),
+           ),
+           Divider(
+             color: Color(0xffff8a30),
+             height: 40,
+             thickness: 8,
+             indent: 30,
+             endIndent: 10,
+           ),
+           Padding(
+             padding: const EdgeInsets.only(left:30, top: 15, bottom: 15),
+             child: Text(
+               "Headline", style: TextStyle(
+               fontWeight: FontWeight.bold,
+               fontSize: 20
+             ),
+             ),
+           ),
+           Expanded(child: NewsGrid(
+             articles: listVieModel.articles,
+           ))
+         ],
+        ),
+      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'News Flash',
       theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xfffefdfd),
+        appBarTheme: AppBarTheme(
+          color: Color(0xfffefdfd),
+          elevation: 0,
+          textTheme: TextTheme(
+            title: TextStyle(
+              color: Colors.black
+            )
+          ),
+          actionsIconTheme: IconThemeData(
+            color: Colors.black
+          )
+        ),
       ),
       home: MultiProvider(
         providers: [ChangeNotifierProvider(
