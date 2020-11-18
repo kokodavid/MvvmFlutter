3c97349fea2785c9ee083a66ea361dfe0225cc89
David mochoge <47295684+kokodavid@users.noreply.github.com>
2020-11-18 10:04:37 +0300

    Models (#1)
    * news models
    
    * news models

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/Screens/news_screen.dart';
+import 'package:newsapp/viewModels/news_article_list_view_model.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News Flash',
+      theme: ThemeData(
+      ),
+      home: MultiProvider(
+        providers: [ChangeNotifierProvider(
+          create: (_) => NewsArticleListViewModel(),
+        )],
+          child: NewsScreen()
+      ),
+    );
+  }
+}
+
