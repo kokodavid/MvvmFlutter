57f759aa5a7f73e0113dec9b4a01bbe59de0e19c
David mochoge <47295684+kokodavid@users.noreply.github.com>
2020-11-21 20:30:40 +0300

    news detail main page (#4)
    * news detail main page
    
    * news detail main page

--- lib/widgets/news_grid.dart
+++ lib/widgets/news_grid.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/Screens/news_article_details.dart';
 import 'package:newsapp/viewModels/news_article_view_model.dart';
 import 'package:newsapp/widgets/circle_image.dart';
 
@@ -7,27 +8,36 @@ class NewsGrid extends StatelessWidget {
 
   NewsGrid({this.articles});
 
+  void _showNewsArticlesDetails(context, article) {
+    Navigator.push(context, MaterialPageRoute(builder: (_) {
+      return NewsArticleDetailScreen(article: article,);
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       gridDelegate:
-      SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
       itemBuilder: (context, index) {
         var article = articles[index];
-        return GridTile(
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            child: CircleImage(
-              imageUrl: article.urlToImage,
+        return GestureDetector(
+          onTap: () {
+            _showNewsArticlesDetails(context, article);
+          },
+          child: GridTile(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: CircleImage(
+                imageUrl: article.urlToImage,
+              ),
             ),
-          ),
-          footer: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Text(article.title,style: TextStyle(
-              fontWeight: FontWeight.bold
-            ),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis
+            footer: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Text(article.title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis),
             ),
           ),
         );
